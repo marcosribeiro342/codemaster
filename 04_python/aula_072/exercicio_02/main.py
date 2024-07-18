f1b32e0b223c0c789d7647bf8a34bab5b56e0881
marcosribeiro342 <marcos.r.342@gmail.com>
2024-07-18 20:57:26 +0100

    v2-18/07/2024

--- 04_python/aula_072/exercicio_02/main.py
+++ 04_python/aula_072/exercicio_02/main.py
@@ -5,7 +5,7 @@ limpa()
 lista_original =["uva", "maca","morango","ananás", "banana", "laranja"]
 print(lista_original)
 print(f"O primeiro elemento é: {lista_original[0]}")
-print(f"O ultimo elemento é: {lista_original[5]}")
+print(f"O ultimo elemento é: {lista_original[len(lista_original)-1]}")
 print(f"O ultimo elemento é: {lista_original[-1]}")
 print(f"A sublista contendo do 2º até ao 4º elemento {lista_original[1:4]}")
 lista_original[1] ="clementina"
